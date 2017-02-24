f52cb5c56d7137b3fe74ada487167aa79bccc251
William Ahrons <william.ahrons@gmail.com>
2017-02-24 20:46:05 -0300

    Add first chart to Main Page

--- server_python/server.py
+++ server_python/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,13 +11,28 @@ engine = create_engine('sqlite:///database.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 
+
 @app.route("/health-check")
 def health_check():
     return "200"
 
-@app.route("/temperature", methods=['POST'])
+@app.route('/index')
+def root():
+     return app.send_static_file('index.html')
+
+@app.route('/temperature')
+def temperatureControl():
+    temperature = "0"
+    session = DBSession()
+    for row in session.query(Temperature).all():
+        print(row.temperature)
+        temperature = temperature + "," +  str(row.temperature) 
+    return render_template('temperature.html', temperature=temperature) 
+
+@app.route("/temperature-1", methods=['POST'])
 def temperature():
     temp = request.form['temp']
+    print " Temperature "+ temp
     session = DBSession()
     session.add(Temperature(temperature=temp))
     session.commit()
@@ -24,3 +40,4 @@ def temperature():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
