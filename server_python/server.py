de25e16df4b5e7871de43de8502e19fd854b5a97
William Ahrons <william.ahrons@gmail.com>
2017-02-28 22:23:10 -0300

    Change API url and param

--- server_python/server.py
+++ server_python/server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import jsonify
 
 import math
 import statistics
@@ -34,7 +35,7 @@ def average(list):
 def standardDeviation(average,list):
    return statistics.pstdev(list)
 
-@app.route('/temperature')
+@app.route('/temperature', methods=['GET'])
 def temperatureControl():
     temperature = "0"
     time = '0:00:00'
@@ -52,14 +53,15 @@ def temperatureControl():
     return render_template('temperature.html', temperature=temperature,
             time=time.split(','), average = average2,standardDeviation = sd) 
 
-@app.route("/temperature-1", methods=['POST'])
+@app.route("/temperature", methods=['POST'])
 def temperature():
-    temp = request.form['temp']
-    print " Temperature "+ temp
+    json = request.get_json()
+    temp = json['temp']
+    print " Temperature "+ str(temp)
     session = DBSession()
     session.add(Temperature(temperature=temp, time=datetime.now()))
     session.commit()
-    return temp
+    return str(temp) 
 
 if __name__ == '__main__':
     app.run(debug=True)
